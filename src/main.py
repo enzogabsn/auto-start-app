83010237eb5c934cb3c850d963b5ca686be40efd
EnzinCoda <enzogabsn@gmail.com>
2025-06-10 18:32:56 -0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,35 @@
+from os import system
+from time import sleep
+import pyautogui
+
+pyautogui.press("win")
+sleep(1)
+pyautogui.typewrite("Roblox Studio", interval=0.01)
+sleep(0.5)
+pyautogui.press("enter")
+sleep(6)
+pyautogui.moveTo(x=27, y=6, duration=0.5)
+sleep(1)
+pyautogui.dragTo(x=20, y=1095, duration=0.5)
+sleep(1)
+pyautogui.moveTo(x=700, y=1094, duration=0.2)
+sleep(0.3)
+pyautogui.click()
+
+
+pyautogui.press("win")
+sleep(1)
+pyautogui.typewrite("Discord", interval=0.01)
+sleep(0.5)
+pyautogui.press("enter")
+sleep(25)
+pyautogui.moveTo(x=1830, y=20, duration=0.5)
+sleep(1)
+pyautogui.click()
+
+
+pyautogui.press("win")
+sleep(1)
+pyautogui.typewrite("Obsidian", interval=0.01)
+sleep(0.5)
+pyautogui.press("enter")
